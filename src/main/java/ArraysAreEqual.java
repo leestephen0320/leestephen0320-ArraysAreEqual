422f02840bf0b9b88ad810278b0e6b3204a8bea0
leestephen0320 <leestephen0320@gmail.com>
2023-02-18 22:28:47 +0000

    modified:   src/main/java/ArraysAreEqual.java

--- src/main/java/ArraysAreEqual.java
+++ src/main/java/ArraysAreEqual.java
@@ -15,6 +15,13 @@ public class ArraysAreEqual {
      * @return true if the values of a are equal to the values of b.
      */
     public boolean equal(int[] a, int[] b){
-        return false;
+        if(a.length == b.length){
+            for(int i = 0; i < a.length; i++){
+                if(a[i] != b[i]){
+                    return false;
+                }
+            }
+            return true;
+        }else{return false;}
     }
 }
